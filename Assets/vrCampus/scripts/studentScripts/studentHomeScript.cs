f89af0cefc34accb7b4bb43fe79a9434d027d45d
Houssam <houssam.naous1@gmail.com>
2020-12-20 16:10:39 +0100

    subscribed to InputTracking events

--- Assets/vrCampus/scripts/studentScripts/studentHomeScript.cs
+++ Assets/vrCampus/scripts/studentScripts/studentHomeScript.cs
@@ -5,21 +5,110 @@ using vrCampusCourseware;
 
 namespace VRCampus
 {
-
     public class studentHomeScript : MonoBehaviour
     {
         [Header("Courseware")]
         [SerializeField]
         TrackingDisplay leftScreen;
 
+        [SerializeField] private Transform m_Camera;
+        [SerializeField] private float m_RayLength = 500f;
+        [SerializeField] private Reticle m_Reticle;
+
+        private bool enable = false;
+        public void enableTeacherScripts(bool mode)
+        {
+            enable = mode;
+        }
+        private gazeableObject m_CurrentGazeable;
+        private gazeableObject m_LastGazeable;
+
+        switchRooms.roomType curRoom = switchRooms.roomType.Study;
+        public void switchRoom(switchRooms.roomType type)
+        {
+            curRoom = type;
+        }
+
+        private void EyeRaycast()
+        {
+            // Create a ray that points forwards from the camera.
+            Ray ray = new Ray(m_Camera.position, m_Camera.forward);
+            RaycastHit hit;
+
+            // Do the raycast forweards to see if we hit an interactive item
+            if (Physics.Raycast(ray, out hit, m_RayLength))
+            {
+                gazeableObject interactible = hit.collider.GetComponent<gazeableObject>();
+                //attempt to get the VRInteractiveItem on the hit object
+                m_CurrentGazeable = interactible;
+
+                // If we hit an interactive item and it's not the same as the last interactive item, then call Over
+                if (interactible && interactible != m_LastGazeable)
+                {
+                    interactible.Over();
+                }
+
+                // Deactive the last interactive item 
+                if (interactible != m_LastGazeable)
+                    DeactiveateLastInteractible();
+
+                m_LastGazeable = interactible;
+
+                if (interactible)
+                {
+                    m_Reticle.SetOn(true);
+                }
+                else
+                {
+                    m_Reticle.SetOn(false);
+                }
+
+                // Something was hit, set at the hit position.
+                if (m_Reticle)
+                    m_Reticle.SetPosition(hit);
+
+            }
+            else
+            {
+                // Nothing was hit, deactive the last interactive item.
+                DeactiveateLastInteractible();
+                m_CurrentGazeable = null;
+                m_Reticle.SetOn(false);
+
+                // Position the reticle at default distance.
+                if (m_Reticle)
+                    m_Reticle.SetPosition();
+            }
+        }
+
+        // Update is called once per frame
         void Update()
         {
-            
+            if (!enable) return;
+            EyeRaycast();
         }
 
-        public void switchRoom(switchRooms.roomType rt)
+        private void DeactiveateLastInteractible()
         {
+            if (m_LastGazeable == null)
+                return;
 
+            m_LastGazeable.Out();
+            m_LastGazeable = null;
         }
+/*
+        private void HandleUp()
+        {
+            if (m_CurrentGazeable != null)
+                m_CurrentGazeable.Up();
+        }
+
+
+        private void HandleDown()
+        {
+            if (m_CurrentGazeable != null)
+                m_CurrentGazeable.Down();
+        }
+        */
     }
 }
--- Assets/vrCampus/scripts/studentScripts/studentTrackingScript.cs
+++ Assets/vrCampus/scripts/studentScripts/studentTrackingScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
@@ -9,10 +10,99 @@ public class studentTrackingScript : MonoBehaviour {
 
     [Header("Courseware")]
     [SerializeField]
-    TrackingDisplay trackingScreen;
+    TrackingDisplay leftScreen;
+
+    Vector3 headsetPos = Vector3.zero;
+    Quaternion headsetRot = Quaternion.identity;
+    Vector3 controllerPos = Vector3.zero;
+    Quaternion controllerRot = Quaternion.identity;
+
+    string deviceName = "unnamed";
+    bool hasController = false;
+    float deltaTime;
+
+    void Awake() {
+        var headDevices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.Head, headDevices);
+
+        if(headDevices.Count == 1)
+        {
+            InputDevice device = headDevices[0];
+            //Debug.Log(string.Format("Device name '{0}' with role '{1}'", device.name, device.role.ToString()));
+            deviceName = device.name;
+        }
+
+        // Subscribe to Input Tracking Events
+        InputTracking.nodeAdded += inputNodeAdded;
+        InputTracking.nodeRemoved += inputNodeRemoved;
+        InputTracking.trackingAcquired += inputTrackingAcquired;
+        InputTracking.trackingLost += inputTrackingLost;
+
+    }
+
+    void inputNodeAdded(XRNodeState state)
+    {
+        leftScreen.TrackingEvent(TrackingDisplay.TrackingEventType.nodeAdded, state);
+    }
+
+    void inputNodeRemoved(XRNodeState state)
+    {
+        leftScreen.TrackingEvent(TrackingDisplay.TrackingEventType.nodeRemoved, state);
+    }
+
+    void inputTrackingAcquired(XRNodeState state)
+    {
+        leftScreen.TrackingEvent(TrackingDisplay.TrackingEventType.trackingAcquired, state);
+    }
+
+    void inputTrackingLost(XRNodeState state)
+    {
+        leftScreen.TrackingEvent(TrackingDisplay.TrackingEventType.nodeRemoved, state);
+    }
+    
 
     // Once you complete this module, we'll keep your Update function active
     // to drive the map display
     void Update () {
+
+        List<XRNodeState> nodeStates = new List<XRNodeState>();
+        InputTracking.GetNodeStates(nodeStates);
+        foreach(XRNodeState nstate in nodeStates)
+        {
+            if(nstate.nodeType ==  XRNode.Head)
+            {
+                nstate.TryGetPosition(out headsetPos);
+                nstate.TryGetRotation(out headsetRot);
+            }else if(nstate.nodeType == XRNode.GameController)
+            {
+                nstate.TryGetPosition(out controllerPos);
+                nstate.TryGetRotation(out controllerRot);
+                hasController = true;
+            }
+        }
+
+        // Calculate FPS
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+
+        // leftScreen.DebugInfo(headsetPos.ToString());
+        // This is the fail case, where there was no center eye was available.
+        //rotation = Quaternion.identity;
+        
+        leftScreen.TrackingInfo(deviceName,
+                                TrackingSpaceType.RoomScale,
+                                headsetPos,
+                                headsetRot,
+                                hasController,
+                                controllerPos,
+                                controllerRot,
+                                fps,
+                                1.0f);
+
+        leftScreen.DebugInfo(headsetPos.ToString());
+
     }
+ 
+
+
 }
